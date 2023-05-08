15dc927d81c6726de0b2fd78c8619a30b3d06ec6
MichalOkon <michalionis123@gmail.com>
2023-05-08 22:25:29 +0200

    Add new classes for products and granular strategies

--- Products/Product.py
+++ Products/Product.py
@@ -1,105 +1,24 @@
-import numpy as np
-
-from datamodel import OrderDepth, Order
+from Products.Strategy import Strategy, FixedStrategy, CrossStrategy, DiffStrategy
 
 
-class Product:
-    def __init__(self, name: str, prod_position: int, new_buy_orders: int, new_sell_orders: int):
-        self.name = name
-        self.cached_prices: list = []
-        self.cached_means: list = []
-        self.max_pos: int = 0
-
-        self.prod_position: int = prod_position
-        self.new_buy_orders: int = new_buy_orders
-        self.new_sell_orders: int = new_sell_orders
-
-    def sell_product(self, best_bids, i, order_depth, orders):
-        # Sell product at best bid
-        best_bid_volume = order_depth.buy_orders[best_bids[i]]
-        if self.prod_position - best_bid_volume >= -self.max_pos:
-            orders.append(Order(self.name, best_bids[i], -best_bid_volume))
-            self.prod_position += -best_bid_volume
-            self.new_sell_orders += best_bid_volume
-
-        else:
-            # Sell as much as we can without exceeding the self.max_pos[product]
-            vol = self.prod_position + self.max_pos
-            orders.append(Order(self.name, best_bids[i], -vol))
-            self.prod_position += -vol
-            self.new_sell_orders += vol
-
-    def buy_product(self, best_asks, i, order_depth, orders):
-        # Buy product at best ask
-        best_ask_volume = order_depth.sell_orders[best_asks[i]]
-        if self.prod_position - best_ask_volume <= self.max_pos:
-            orders.append(Order(self.name, best_asks[i], -best_ask_volume))
-            self.prod_position += -best_ask_volume
-            self.new_buy_orders += -best_ask_volume
-        else:
-            # Buy as much as we can without exceeding the self.max_pos[product]
-            vol = self.max_pos - self.prod_position
-            orders.append(Order(self.name, best_asks[i], vol))
-            self.prod_position += vol
-            self.new_buy_orders += vol
+class Pearls(FixedStrategy):
+    def __init__(self):
+        super().__init__("PEARLS", max_pos=20)
 
 
-class DiffStrategy(Product):
-    def __init__(self, derivative_resolution: int, diff_thresh: int, trade_count: int, name: str, prod_position: int,
-                 new_buy_orders: int, new_sell_orders: int):
-        super().__init__(name, prod_position, new_buy_orders, new_sell_orders)
-        self.derivative_resolution: int = derivative_resolution
-        self.diff_thresh: int = diff_thresh
-        self.trade_count: int = trade_count
+class Bananas(CrossStrategy):
+    def __init__(self):
+        super().__init__("BANANAS", min_req_price_difference=3, max_position=20)
 
-    def trade(self, order_depth: OrderDepth, orders: list):
-        self.calculate_means()
 
-        diff = self.get_price_difference()
+class PinaColadas(DiffStrategy):
+    def __init__(self):
+        super().__init__("PINA_COLADAS", max_position=150, derivative_resolution=150, diff_thresh=30)
 
-        if diff < -self.diff_thresh and len(order_depth.sell_orders) != 0:
-            best_asks = sorted(order_depth.sell_orders.keys())
 
-            i = 0
-            while i < self.trade_count and len(best_asks) > i:
-                if self.prod_position == self.max_pos:
-                    break
-                self.buy_product(best_asks, i, order_depth, orders)
-                i += 1
-
-        if diff > self.diff_thresh and len(order_depth.buy_orders) != 0:
-            best_bids = sorted(order_depth.buy_orders.keys(), reverse=True)
-
-            i = 0
-            while i < self.trade_count and len(best_bids) > i:
-                if self.prod_position == -self.max_pos:
-                    break
-                self.sell_product(best_bids, i, order_depth, orders)
-
-                i += 1
-
-    def get_price_difference(self):
-        # Calculate the difference between the current mean and the mean from
-        # self.derivative_resolution days ago
-        if len(self.cached_means) < self.derivative_resolution + 1:
-            old_mean = self.cached_means[0]
-        else:
-            old_mean = self.cached_means[-self.derivative_resolution]
-        diff = self.cached_means[-1] - old_mean
-        return diff
-
-    def calculate_means(self):
-        # 
-        if len(self.cached_prices) == 0:
-            self.cached_means.append(0)
-
-        else:
-            relevant_prices = []
-            for day_prices in self.cached_prices[max(-len(self.cached_prices), -1):]:
-                for price in day_prices:
-                    relevant_prices.append(price)
-            prices = np.array([x[1] for x in relevant_prices])
-            quantities = np.abs(np.array([x[0] for x in relevant_prices]))
-
-            self.cached_means.append(np.average(prices, weights=quantities))
-
+class Baguette(DiffStrategy):
+    def __init__(self):
+        super().__init__("BAGUETTE", max_position=150, derivative_resolution=20, diff_thresh=200)
+class Dip(DiffStrategy):
+    def __init__(self):
+        super().__init__("DIP", max_position=150, derivative_resolution=20, diff_thresh=200)
