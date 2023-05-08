ba53ffc6d83ab5d7774fbf68726611060ca93a56
MichalOkon <michalionis123@gmail.com>
2023-05-08 23:52:16 +0200

    Update simulator.py

--- simulator/simulator.py
+++ simulator/simulator.py
@@ -8,6 +8,7 @@ from matplotlib import pyplot as plt
 from datetime import datetime
 
 
+# Instructions on how to run this simulator can be found in the readme file
 class Simulator():
     def __init__(self, prices_round: str, trades_round: str, trader):
         self.prices_round_name = prices_round
@@ -26,8 +27,9 @@ class Simulator():
         self.total_pnl = {}
 
     def simulate(self):
-        own_trades = {}
+        # Simulates one round of the trading game
 
+        own_trades = {}
         unique_timestamps = self.prices["timestamp"].unique()
 
         for timestamp in tqdm(unique_timestamps):
@@ -37,24 +39,23 @@ class Simulator():
             last_result = self.trader.run(state)
             # Simulate the market
             last_prices = self.prices[self.prices['timestamp'] == timestamp]
-            own_trades = self.compute_trades(last_prices, last_result)
+            own_trades = self.process_trades(last_prices, last_result)
             # Calculate the profits
-            self.compute_position_profit(own_trades)
+            self.process_position_profit(own_trades)
             self.calculate_pnl(last_prices)
 
         self.plot_pnl()
         self.plot_positions()
 
-    def load_trading_sate(self, timestamp, own_trades):
-        # Get the timestamp
+    def load_trading_sate(self, timestamp, own_trades=None):
+        # Creates a new trading state given the timestamp and the own trades that have been performed in the last round
 
         curr_prices = self.prices[self.prices['timestamp'] == timestamp]
         listings = {}
         order_depths = {}
         market_trades = {}
         observations = {}
-        positions = {}
-        # Get listings
+
         for prod_row in curr_prices.iterrows():
             # Get the symbol of the product
             product = prod_row[1]["product"]
@@ -94,8 +95,6 @@ class Simulator():
                 Trade(traded_row[1]['symbol'], traded_row[1]['price'], traded_row[1]['quantity'],
                       traded_row[1]['buyer'], traded_row[1]['seller'], timestamp))
 
-        # Set position to zeros if the dictionary is empty
-
         state = TradingState(
             timestamp=timestamp,
             listings=listings,
@@ -108,33 +107,39 @@ class Simulator():
 
         return state
 
-    def compute_trades(self, last_prices, last_result):
+    def process_trades(self, last_prices, last_result):
         # Computes if any trades have been performed and outputs a dictionary containing a list of traded products
+
         own_trades = {}
         for product in last_result.keys():
 
             own_trades[product] = []
             product_orders = last_result[product]
             product_row = last_prices[last_prices["product"] == product]
+
             for order in product_orders:
                 quantity = order.quantity
                 price = order.price
+
                 if order.quantity < 0:
                     # Calculate if the product was indeed sold
                     for i in [1, 2, 3]:
-                        if product_row[f"bid_price_{i}"].item() == product_row[f"bid_price_{i}"].item() and product_row[
-                            f"bid_price_{i}"].item() >= price and quantity != 0:
+                        if product_row[f"bid_price_{i}"].item() == product_row[f"bid_price_{i}"].item() \
+                                and product_row[f"bid_price_{i}"].item() >= price \
+                                and quantity != 0:
                             sold_quantity = max(quantity, -product_row[f"bid_volume_{i}"].item())
                             own_trades[product].append(
                                 Trade(product, product_row[f"bid_price_{i}"].item(), sold_quantity, "", "",
                                       product_row["timestamp"].item()))
                             # Update remaining quantity
                             quantity = min(0, product_row[f"bid_volume_{i}"].item() + quantity)
+
                 if order.quantity > 0:
                     # Calculate if the product was indeed bought
                     for i in [1, 2, 3]:
-                        if product_row[f"ask_price_{i}"].item() == product_row[f"ask_price_{i}"].item() and product_row[
-                            f"ask_price_{i}"].item() <= price and quantity != 0:
+                        if product_row[f"ask_price_{i}"].item() == product_row[f"ask_price_{i}"].item() \
+                                and product_row[f"ask_price_{i}"].item() <= price \
+                                and quantity != 0:
                             bought_quantity = min(quantity, product_row[f"ask_volume_{i}"].item())
                             own_trades[product].append(
                                 Trade(product, product_row[f"ask_price_{i}"].item(), bought_quantity, "", "",
@@ -144,7 +149,9 @@ class Simulator():
 
         return own_trades
 
-    def compute_position_profit(self, traded_products):
+    def process_position_profit(self, traded_products):
+        # Calculates and update the position and profit change given the trades made by the agent
+
         for product in traded_products.keys():
             product_trades = traded_products[product]
 
@@ -168,6 +175,7 @@ class Simulator():
 
     def calculate_pnl(self, curr_prices):
         # Calculates the current total profit and loss given the products' prices, positions and money profit
+
         pnl = {}
         for prod_row in curr_prices.iterrows():
             product = prod_row[1]["product"]
@@ -179,10 +187,13 @@ class Simulator():
             self.total_pnl[product].append(pnl[product])
         return pnl
 
+    # PLOTTING FUNCTIONS
     def plot_pnl(self):
-        if not os.path.exists("../pnl"):
-            os.makedirs("../pnl")
-        # Plots the profit and loss
+        # Plots the profit and loss after the game has been finished
+
+        if not os.path.exists("pnl"):
+            os.makedirs("pnl")
+
         for prod in self.total_pnl.keys():
             plt.plot(self.total_pnl[prod], label=prod)
             plt.legend()
@@ -193,18 +204,25 @@ class Simulator():
         # plt.savefig("pnl.jpg")
 
     def plot_midprices(self):
-        if not os.path.exists("../midprices"):
-            os.makedirs("../midprices")
+        # Plots mid_prices found in the csv file
+
+        if not os.path.exists("midprices"):
+            os.makedirs("midprices")
+
         unique_prods = self.prices["product"].unique()
         for product in unique_prods:
             prod_rows = self.prices[self.prices["product"] == product]
+            # print(prod_rows["mid_price"].reset_index())
             plt.plot(prod_rows["mid_price"].reset_index(drop=True))
             plt.savefig(f"midprices/mid_price_{product}_{self.prices_round_name.replace('/', '_')}.jpg")
             plt.clf()
 
     def plot_positions(self):
-        if not os.path.exists("../positions"):
-            os.makedirs("../positions")
+        # Plots positions of the products after the game has been finished
+
+        if not os.path.exists("positions"):
+            os.makedirs("positions")
+
         unique_prods = self.prices["product"].unique()
         for product in unique_prods:
             plt.plot(self.position_history[product])
