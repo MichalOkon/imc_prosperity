3851700f95899ca7fd6e133e63323c8da970d7a9
Kuba Tokarz <j.m.tokarz@student.tudelft.nl>
2023-05-09 13:39:40 +0200

    Clean-up of simulator folder, optimize and product

--- simulator/simulator_test.py
+++ simulator/simulator_test.py
@@ -1,12 +1,14 @@
-from simulator import Simulator
-import sys
 import importlib
+import sys
+
+from simulator import Simulator
 
 
 def main():
     trader_file = importlib.import_module(sys.argv[1])
     trader = trader_file.Trader()
     sim = Simulator(sys.argv[2], sys.argv[3], trader)
+
     # Use this to function to start the simulation
     sim.simulate()
     sim.plot_midprices()
